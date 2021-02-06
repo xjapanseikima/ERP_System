7aea4dc1e99a43292cca3eb7f6b7c1e1cfa03c7f
Albert <albert83410@gmail.com>
2021-02-06 18:23:54 -0500

    product function created!

--- src/main/java/com/example/commutronics/dao/ProductRepository.java
+++ src/main/java/com/example/commutronics/dao/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.commutronics.dao;
 
-public class ProductRepository {
+import com.example.commutronics.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
--- src/main/java/com/example/commutronics/service/ProductService.java
+++ src/main/java/com/example/commutronics/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.example.commutronics.service;
 
+
+import com.example.commutronics.dao.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository repo;
+    public List getAllProducts(){
+        return repo.findAll();
+    }
 }
+
+
