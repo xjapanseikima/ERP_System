3a4cc3cef07c2744ff451a619c032e05aa83b357
unknown <albert83410gmail.com>
2021-02-05 21:59:30 -0500

    company function

---
+++ src/main/java/com/example/commutronics/model/Product.java
@@ -0,0 +1,33 @@
+package com.example.commutronics.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "product_id")
+    private int product_id;
+    @Column(name = "product_name")
+    private String product_name;
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+}
