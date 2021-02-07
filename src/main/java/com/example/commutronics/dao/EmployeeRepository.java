518b65a949a6c56b4ac3b8a40bd245c3666ee033
xjapanseikima <magican213@gmail.com>
2021-02-06 18:08:39 -0800

    big fixed

--- src/main/java/com/example/commutronics/dao/EmployeeRepository.java
+++ src/main/java/com/example/commutronics/dao/EmployeeRepository.java
@@ -1,16 +1,12 @@
 package com.example.commutronics.dao;
 
 import com.example.commutronics.model.Employee;
-import com.example.commutronics.model.Transcation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
