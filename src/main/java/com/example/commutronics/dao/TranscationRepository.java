357ac06759f6a4e8c3c52fe8d4614e16fa15c90e
xjapanseikima <magican213@gmail.com>
2021-02-06 16:49:11 -0800

    delete all useless class

--- src/main/java/com/example/commutronics/dao/TranscationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.commutronics.dao;
-
-import com.example.commutronics.model.Transcation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface TranscationRepository extends JpaRepository<Transcation, Integer> {
-    Transcation findByInvoiceid(String id);
-}
