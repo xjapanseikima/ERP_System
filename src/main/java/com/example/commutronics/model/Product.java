35b9c68694efd99cdc704a2f25d52f1eb67f8c81
xjapanseikima <magican213@gmail.com>
2021-02-12 15:28:48 -0800

    relationship between item_record and Product

--- src/main/java/com/example/commutronics/model/Product.java
+++ src/main/java/com/example/commutronics/model/Product.java
@@ -1,6 +1,5 @@
 package com.example.commutronics.model;
 
-
 import javax.persistence.*;
 import java.util.List;
 
@@ -9,16 +8,25 @@ import java.util.List;
 public class Product {
     @Id
     @Column(name = "product_id")
-    private int product_id;
+    private String product_id;
     @Column(name = "product_name")
     private String product_name;
+    @OneToMany(mappedBy="product",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+    private java.util.List<Item_record> itemList;
 
+    public List<Item_record> getItemList() {
+        return itemList;
+    }
 
-    public int getProduct_id() {
+    public void setItemList(List<Item_record> itemList) {
+        this.itemList = itemList;
+    }
+
+    public String getProduct_id() {
         return product_id;
     }
 
-    public void setProduct_id(int product_id) {
+    public void setProduct_id(String product_id) {
         this.product_id = product_id;
     }
 
